ef05c581225a13b1f096c996c77c65f90590bc2d
batoolalali <abdullahbatool93@gmail.com>
2020-02-05 12:45:52 +0200

    Summary

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var conti;
 var movie= ' is a good Choice';
-var sugg;
+
 var c;
 
 if (confirm("Press Ok to continue"))
@@ -15,23 +15,38 @@ var yourmovie = prompt("enter your favorite movie type; crime, drama or comedy")
 document.write('<spam>'+yourmovie+'</spam>');
 document.write('<spam>'+movie+'</spam>');
 
-if (yourmovie == 'crime' )
+
+function GiveSug(YourM)
 {
-    sugg= '  !  watch Bridge  of Spies is a good crime movie'}
-    else if (yourmovie == 'drama')
-    {
-        sugg= '!  watch Wonder is a good drama movie';
+    var sugg;
+if (YourM== 'crime' )
+{
+    document.write( " !  watch Bridge  of Spies is a good crime movie" );
+    
+    return YourM;
+
+
+}
+    else if (YourM == 'drama')
+    {   
+        document.write( " !   watch Wonder is a good drama movie" );
+        return YourM;
+
     }
-    else if (yourmovie== 'comedy' )
-    {
-        sugg= '  !  watch Jumanji is a good comedy movie';
+    else if (YourM== 'comedy' )
+    {   
+        document.write( " !  watch Jumanji is a good comedy movie" );
+        return YourM;
+
     }
     else {
-        sugg='Wronge input';
-    }
+        document.write( " Wronge input" );
 
-    
-    document.write('<spam>'+sugg+'</spam>');
+    }
+}
+
+    GiveSug(yourmovie);
+
 
     var color = prompt("PLease enter the color for this page.")
     document.body.style.background= color;tyle.backgroundColor = color;
